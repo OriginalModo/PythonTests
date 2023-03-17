fd8f1bb6015b5a05bb176928173695b565418fb1
zonef <solenuy4@mail.ru>
2023-03-17 20:21:29 +0300

    TOKEN: add few tests

--- tests/test_calculator_pytest.py
+++ tests/test_calculator_pytest.py
@@ -1,10 +1,11 @@
 import pytest
 
-from lesson.calculator import calculator
+from lesson.calculator import calculator, plus
 
 
-def test_plus():
+def test_calc_plus():
     assert calculator('2+2') == 4
+
 
 def test_no_signs():
     with pytest.raises(ValueError) as error:
@@ -16,6 +17,18 @@ def test_two_signs():
     with pytest.raises(ValueError) as e:
         calculator('2+2+2')
     assert 'Ошибка: 2 целых числа' == e.value.args[0]
+
+
+def test_plus():
+    assert plus(2, 2) == 4
+
+
+def test_plus_zeroes():
+    assert plus(0, 0) == 0
+
+def test_plus_zeroes_negative():
+    assert plus(1, -1) == 0
+
 
 if __name__ == '__main__':
     pytest.main()
